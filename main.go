65386a3e2686eaf5f1110b1537e9ced6ac2dd2a4
Ellieanna Ross <74512916+ellieannar@users.noreply.github.com>
2024-03-23 12:55:17 -0700

    added prometheus metrics

--- main.go
+++ main.go
@@ -6,6 +6,10 @@
 	/bar handles GET and DELETE.
 		For GET, return the current timestamp,
 		for DELETE, require that the user passes a json struct that looks like {"name": insert name here}, then returns "Hello" plus the name provided.
+
+	Add 2 metrics to your rest api.
+		A metric named "Up", while the program is running, it randomly either pushes a 0 or a 1 every 60 seconds
+		A metric that counts every time a GET is called on the /foo endpoint
 */
 
 package main
@@ -14,14 +18,36 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"math/rand"
 	"net/http"
+	"sync"
 	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var mu sync.Mutex
+
+var up = prometheus.NewGauge(
+	prometheus.GaugeOpts{
+		Name: "up_time",
+		Help: "Pushes 1 or 0 every 60 seconds while running",
+	},
+)
+
+var getCount = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Name: "get_request_count",
+		Help: "Tracks the total number of times GET is called on the /foo endpoint",
+	},
 )
 
 func fooHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
 		response := map[string]string{"foo": "Get"}
+		getCount.Inc()
 		json.NewEncoder(w).Encode(response)
 
 	case http.MethodPost:
@@ -56,9 +82,26 @@ func barHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func UpFunction() {
+	go func() {
+		for {
+			mu.Lock()
+			up.Set(float64(rand.Intn(2))) // Set up to be either 0 or 1 randomly
+			mu.Unlock()
+			time.Sleep(60 * time.Second)
+		}
+	}()
+
+}
+
 func main() {
+	prometheus.Register(getCount)
+	prometheus.Register(up)
+
+	UpFunction()
+
 	http.HandleFunc("/foo", fooHandler)
 	http.HandleFunc("/bar", barHandler)
-
+	http.Handle("/metrics", promhttp.Handler())
 	http.ListenAndServe(":3333", nil)
 }
